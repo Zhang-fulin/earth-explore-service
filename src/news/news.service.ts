03cba3761274fa1447eba71d3ab5adf77720421d
zhangfulin <1148112937@qq.com>
2025-08-06 19:14:10 +0800

    [add news/today api]

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -17,13 +17,11 @@ export class NewsService {
     console.log('Fetching today\'s news...');
     const now = new Date();
     const startOfDay = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
-    const startOfNextDay = new Date(startOfDay.getTime() + 24 * 60 * 60 * 1000);
-
+    const currentDateISOString = startOfDay.toISOString();
     const { data, error } = await this.supabase
       .from('cctv-news')
       .select('*')
-      .gte('time', startOfDay.toISOString())
-      .lt('time', startOfNextDay.toISOString());
+      .gte('time', currentDateISOString);
 
     if (error) {
       console.error('加载新闻失败:', error.message);
